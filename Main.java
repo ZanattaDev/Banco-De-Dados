890d23cdcbc32a1bffec2ffeb35bfcaf9c9bb9fb
Pedro Henrique Zanatta <pedrohztt30@gmail.com>
2025-10-28 14:23:19 -0300

    Update Main.java
    Added a main menu for user options to insert or change a name.

--- Main.java
+++ Main.java
@@ -1,2 +1,22 @@
-429: Too Many Requests
-For more on scraping GitHub and how it may affect your rights, please review our Terms of Service (https://docs.github.com/en/site-policy/github-terms/github-terms-of-service).
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== MENU PRINCIPAL === \n" +
+                " 1 - Inserir nome \n" +
+                " 2 - Alterar nome \n");
+
+        Scanner input = new Scanner(System.in);
+        int opcao = input.nextInt();
+
+        if (opcao == 1) {
+            new Inserir();
+        } else if (opcao ==2) {
+            new Alterar();
+        } else {
+            System.out.println("Opção inválida");
+        }
+        input.close();
+    }
+}
