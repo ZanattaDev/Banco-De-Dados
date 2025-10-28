aafa2043b27d07e66aafbeeb833c2b827c53fad9
Pedro Henrique Zanatta <pedrohztt30@gmail.com>
2025-10-28 14:21:09 -0300

    Implement name alteration feature in Alterar class
    Added user input functionality to alter names.

--- Alterar.java
+++ Alterar.java
@@ -1,2 +1,18 @@
-429: Too Many Requests
-For more on scraping GitHub and how it may affect your rights, please review our Terms of Service (https://docs.github.com/en/site-policy/github-terms/github-terms-of-service).
+import java.util.*;
+
+public class Alterar {
+
+    public Alterar() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite o nome a alterar: ");
+        String antigo = input.nextLine();
+
+        System.out.print("Digite o novo nome: ");
+        String novo = input.nextLine();
+
+        input.close();
+        new Conexao().alterar(antigo, novo);
+
+    }
+}
